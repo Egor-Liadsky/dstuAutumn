83bde1ac2851759ef3ab4e5a388b4411edfe6bf9
Abilka <alex.161@mail.ru>
2022-10-22 02:04:40 +0300

    добавлены методы авторизации

--- server/database/handler.py
+++ server/database/handler.py
@@ -1,3 +1,5 @@
+import typing
+
 import psycopg2
 from server.settings import database as setting
 
@@ -7,10 +9,17 @@ class Db:
                                       password=setting.PASSWORD,
                                       host=setting.HOST,
                                       port=setting.PORT)
+        self.cur = self.connection.cursor()
+    def _select_with_email(self, email: str) -> typing.Tuple[str] or None:
+        self.cur.execute("SELECT user_id FROM users WHERE email=%s", (email,))
+        return self.cur.fetchone()
 
-    def select_password(self, user: str) -> str:
-        pass
+    def _select_with_number(self, number: str) -> typing.Tuple[str] or None:
+        self.cur.execute("SELECT user_id FROM users WHERE phone_number=%s", (number,))
+        return self.cur.fetchone()
 
-    def new_user(self, user: str, secret_key: str, public_key: str, public_name: str) -> str:
+    def new_user(self, user: str, secret_key: str, public_key: str, public_name: str):
+        self.cur.execute("INSERT INTO public.users(email, secret_key, public_name, user_id, phone_number)"
+                         "VALUES ('egor@mail.ru', 'random','Egor Lyadskiy', 42, '+1111');", ())
         pass
 
