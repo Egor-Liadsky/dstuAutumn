4c63453fde7c512d116ea2a4422c0c5c9722670a
Abilka <alex.161@mail.ru>
2022-10-21 21:38:27 +0300

    все перенесено в мейн

--- server/app/flask_app.py
+++ server/app/flask_app.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-from server.setting import flask
+from server.settings import flask
 
 app = Flask(__name__)
 
 
-@app.route('', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def index():
     return None
 
-
-if __name__ == '__main__':
-    app.run(host=flask.HOST_USRL, port=8080)
--- server/main.py
+++ server/main.py
@@ -1,1 +1,6 @@
-import app.flask_error
+from app import flask_app
+import settings
+
+
+if __name__ == '__main__':
+    flask_app.app.run(host=settings.flask.HOST_URL, port=8080)
