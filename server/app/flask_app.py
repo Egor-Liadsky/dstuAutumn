e6adde66f0a59fa88f907be5d53c7ea380bb48ae
Vanya-761 <ivan.romanchenko161@gmail.com>
2022-10-22 06:05:48 +0300

    FIX

--- server/app/flask_app.py
+++ server/app/flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from server.database import handler
+from server.database import requests, handler
 
 app = Flask(__name__)
 
@@ -16,13 +16,11 @@ def index():
     return "Hello world"
 
 
-@app.route('/secret_key', methods=['GET'])
-def secret_key():
-        return {"result": requests.DbOperator().set_secret_key(request.args['secret_key'])}
+#@app.route('/secret_key', methods=['GET'])
+#def secret_key():
+#        return {"result": requests.DbOperator().set_secret_key(request.args['secret_key'])}
 
 
 @app.route('/new_user', methods=['POST'])
 def secret_key():
-        return {"result": requests.DbOperator().new_user(request.args['user'], request.args['secret_key'], request.args['public_key'], request.args['public_name'])}
-
-
+        return {"result": handler.Db().new_user(request.args['user'], request.args['secret_key'], request.args['public_key'], request.args['public_name'])}
